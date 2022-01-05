57188e680cb9c25ead590b68ab25f49a1ae890c5
manuela-agrom <manuela071997@gmail.com>
2022-01-05 16:17:02 +0100

    Operators added but not well implemented

--- ejerciciosJS/interfaceEjs/Calculator/calculator.js
+++ ejerciciosJS/interfaceEjs/Calculator/calculator.js
@@ -1,25 +1,74 @@
 export class Calculator {
-    #acumulate;
+    #accumulated;
+    #auxSecondNumber;
+    #waitingSecondNumber;
+    #operation;
     constructor() {
-        this.#acumulate = '0';
+        this.#accumulated = 0;
+        this.#auxSecondNumber = 0;
+        this.#waitingSecondNumber = false;
+        this.#operation;
     }
-
     checkKey(key) {
-        keyIsNumber = this.isNumber(key);
+        let keyIsNumber = this.isNumber(key);
         if (keyIsNumber) {
-            this.#acumulate = parseFloat(this.#acumulate.concat(key.toString()))
-        } else if () {
-            
+            if (!this.#waitingSecondNumber) {
+                this.#accumulated = parseFloat(this.#accumulated.toString().concat(key));
+            } else {
+                this.#waitingSecondNumber = false;
+                this.#auxSecondNumber = parseFloat(this.#auxSecondNumber.toString().concat(key));
+                return this.#accumulated.toString().concat(' ', this.#operation).concat(' ', this.#auxSecondNumber)
+            }
         } else {
-            return 0;
-        }
-    }
+            if (!this.#waitingSecondNumber) {
+                this.#waitingSecondNumber = true;
+                this.#operation = key;
+                console.log(this.#operation);
+                return this.#accumulated.toString().concat(' ', this.#operation)
+            } else {
+                this.#waitingSecondNumber = false;
+                switch (this.#operation) {
+                    case '+':
+                        this.#accumulated = this.add(this.#accumulated,this.#auxSecondNumber);
+                        break;
+                    case '-':
+                        this.#accumulated = this.subtract(this.#accumulated,this.#auxSecondNumber);
+                        break;
+                    case '/':
+                        this.#accumulated = this.divide(this.#accumulated,this.#auxSecondNumber);
+                        break;
+                    case '*':
+                        this.#accumulated = this.multiply(this.#accumulated,this.#auxSecondNumber);
+                        break;
+                    default:
+                        
+                        break;
+                }
+                console.log(this.#accumulated);
 
+            }
+        }
+        return this.#accumulated;
+    }
     isNumber(key) {
         // if (typeof(key) === 'number') {
         //     return true;
         // }
         // return false;
-        return isNan(parseInt(key))? false : true;
+        let isNumber = isNaN(parseInt(key));
+        return !isNumber;
     }
+    add(number1, number2) {
+        return number1 + number2;
+    }
+    subtract(number1, number2) {
+        return number1-number2;
+    }
+    multiply(number1, number2) {
+        return number1 * number2;
+    }
+    divide(number1, number2) {
+        return number1/number2;
+    }
+
 }
