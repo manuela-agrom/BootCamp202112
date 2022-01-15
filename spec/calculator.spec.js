2077c15d5bf8937d6efa071a68e0cf8fa52616bb
manuela-agrom <manuela071997@gmail.com>
2022-01-15 16:20:02 +0100

    Init testing calculator

---
+++ spec/calculator.spec.js
@@ -0,0 +1,4 @@
+// Lines to add in Jasmine's SpecRunner.html
+// <script src="../../ejerciciosJS/interfaceEjs/Calculator/calculator.js"></script>
+// <script src="../../spec/calculator.spec.js"></script>
+
